3e27cbf48819796874dee58fc08f0819f3b46375
Dimitris Tsioumas <95169128+TDimitris@users.noreply.github.com>
2024-10-18 16:57:53 +0300

    update

--- react/tic-tac-toe/src/App.jsx
+++ react/tic-tac-toe/src/App.jsx
@@ -6,11 +6,16 @@ import GameOver from './components/GameOver';
 
 import { useState } from 'react';
 
-const initialGameBoard = [
+const INITIAL_GAME_BOARD = [
   [null, null, null],
   [null, null, null],
   [null, null, null]
-]
+];
+
+const PLAYERS = {
+  X: 'Player 1',
+  O: 'Player 2'
+}
 
 
 function deriveActivePlayer(gameTurns) {
@@ -23,15 +28,8 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
-
-export default function App() {
-  
-  //const [activePlayer, setActivePlayer] = useState('X');
-  const [gameTurns, setGameTurns] = useState([]);
-
-  let activePlayer = deriveActivePlayer(gameTurns);
-
-  const gameBoard = [...initialGameBoard.map(array => [...array])];
+function deriveGameBoard(gameTurns) {
+  const gameBoard = [...INITIAL_GAME_BOARD.map(array => [...array])];
 
   for (const turn of gameTurns) {
       const {square, player} = turn;
@@ -39,6 +37,11 @@ export default function App() {
 
       gameBoard[row][col] = player;
   };
+
+  return gameBoard;
+}
+
+function deriveWinner(gameBoard, players){
 
   let winner = null;
 
@@ -49,12 +52,27 @@ export default function App() {
     const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
 
     if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol){
-        winner = firstSquareSymbol;
+        winner = players[firstSquareSymbol];
         
     }
     
   };
+  return winner;
+}
 
+
+export default function App() {
+
+  const [players, setPlayers] = useState(PLAYERS)
+  
+  //const [activePlayer, setActivePlayer] = useState('X');
+  const [gameTurns, setGameTurns] = useState([]);
+
+  let activePlayer = deriveActivePlayer(gameTurns);
+
+  const gameBoard = deriveGameBoard(gameTurns);
+
+  const winner = deriveWinner(gameBoard, players);
   const hasDraw = gameTurns.length === 9 && !winner;
 
 
@@ -79,12 +97,31 @@ export default function App() {
 
   };
 
+  function handlePlayerNameChange(symbol, newName){
+
+    setPlayers(prevPlayers => {
+      return{
+        ...prevPlayers,
+        [symbol] : newName//overwritting the name
+      }
+    })
+
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className='highlight-player'>
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer==="X"}/>
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer==="O"}/>
+          <Player initialName={PLAYERS.X}
+                  symbol="X" 
+                  isActive={activePlayer==="X"}
+                  onChangeName={handlePlayerNameChange}
+          />
+          <Player initialName={PLAYERS.O}
+                  symbol="O"
+                  isActive={activePlayer==="O"}
+                  onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard 
--- react/tic-tac-toe/src/components/Player.jsx
+++ react/tic-tac-toe/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Player({initialName, symbol, isActive}) {
+export default function Player({initialName, symbol, isActive, onChangeName}) {
 
     const [ isEditing, setIsEditing ] = useState(false);
 
@@ -8,6 +8,10 @@ export default function Player({initialName, symbol, isActive}) {
 
     function handleEditing(){
         setIsEditing((editing) => !editing);
+
+        if(isEditing) {
+            onChangeName(symbol, playerName);
+        }
     };
 
     function handleChange(evt) {
